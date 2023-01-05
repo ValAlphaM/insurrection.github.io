8e4c4e1a2c64a8a2f16d2989a8ed9f879fe72a6e
ValAlphaM <algobin2003@gmail.com>
2023-01-05 21:18:58 +0100

    Creating repository

---
+++ app.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // SIDEBAR BUTTON ANIMATION
+    // Def section id
+    let playersSection = document.getElementById("players-section");
+    let charactersSection = document.getElementById("characters-section")
+    let mapSection = document.getElementById("map-section");
+    let sectionArray = [playersSection, charactersSection, mapSection]
+
+    // Def button id 
+    let playersSectionButton = document.getElementById("players-button");
+    let charactersSectionButton = document.getElementById("characters-button")
+    let mapSectionButton = document.getElementById("map-button");
+    let buttonArray = [playersSectionButton, charactersSectionButton, mapSectionButton]
+    
+    let map = document.querySelector("#map");
+    let zoom = document.querySelector("#zoom");
+    // Functions
+    const changeSection = (displaySection, displayButton) => {
+        for (let section of sectionArray) {
+            if (section === displaySection){
+                section.style.display = 'block';
+            } else {
+                section.style.display = "none";
+                
+            }
+        };
+        for (let button of buttonArray) {
+            if (button === displayButton) {
+                button.style.backgroundColor = 'rgba(0, 0, 0, 0.25)';
+                button.addEventListener("mouseover", () => {
+                    button.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+                })
+                button.addEventListener("mouseout", () => {
+                    button.style.backgroundColor = "rgba(0, 0, 0, 0.25)";
+                })
+            } else {
+                button.style.backgroundColor = '';
+                button.addEventListener("mouseover", () => {
+                    button.style.backgroundColor = ""
+                })
+                button.addEventListener("mouseout", () => {
+                    button.style.backgroundColor = "";
+                })
+            };
+        };
+    };
+
+    // Button animations
+    playersSectionButton.addEventListener("click", () => changeSection(playersSection, playersSectionButton));
+    charactersSectionButton.addEventListener("click", () => changeSection(charactersSection, charactersSectionButton));
+    mapSectionButton.addEventListener("click", () => changeSection(mapSection, mapSectionButton));
+    map.addEventListener("mouseenter", () => {
+        zoom.style.display = "block";
+        zoom.style.backgroundImage = url(/pictures/map_moonshae_isles.jpg);
+        zoom.style.backgroundSize = "200%";
+    });
+    // ZOOM MAP ANIMATION
+  });
